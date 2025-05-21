3e69471450ed6f9ea443b72794a096fa3c6b4097
DanteE88 <152216561+DanteE88@users.noreply.github.com>
2025-05-21 16:37:11 -0600

    added functions for calibration

---
+++ callibration/run_callibration_exact.py
@@ -0,0 +1,34 @@
+import numpy as np
+import scipy as sp
+
+def aproximate_entropy_params(primes_sample):
+    return 0
+def bin_matrix_params(primes_sample):
+    return 0
+def cumsums_params(primes_sample):
+    return 0
+def excursion_params(primes_sample):
+    return 0
+def excursions_variant_params(primes_sample):
+    return 0
+def frequency_params(primes_sample):
+    return 0
+def linear_complexity_params(primes_sample):
+    return 0
+def longest_run_params(primes_sample):
+    return 0
+def maurer_params(primes_sample):
+    return 0
+def monobit_params(primes_sample):
+    return 0
+def non_overlap_matching_params(primes_sample):
+    return 0
+def overlap_matching_params(primes_sample):
+    return 0
+def runs_params(primes_sample):
+    return 0
+def serial_params(primes_sample):
+    return 0
+def spectral_fft_params(primes_sample):
+    return 0
+
